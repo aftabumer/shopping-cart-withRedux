3a23966d73594ddb0e3116c8ad4b805984ecd76c
aftabumer <innocentboy0300@gmail.com>
2019-08-01 21:02:45 +0500

    set routing

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,25 @@ import { BrowserRouter, Route } from "react-router-dom";
 import MyProducts from "./MyProducts";
 import AddProducts from "./AddProducts";
 import ViewProducts from "./ViewProducts";
+import AddCart from "./AddCart";
+// import ViewProductsInModal from './ViewProductsInModal'
 
-export default class App extends Component {
+class App extends Component {
+ 
+
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ViewProducts} />
-        <Route exact path="/SignIn" component={SignIn} />
+        <Route exact path="/ViewProducts" component={ViewProducts} />
+        <Route exact path="/" component={SignIn} />
         <Route exact path="/SignUp" component={SignUp} />
         <Route exact path="/AddProducts" component={AddProducts} />
         <Route exact path="/MyProducts" component={MyProducts} />
+        <Route exact path="/AddCart" component={AddCart} />
+        {/* <Route exact path="/ViewProductsInModal" component={ViewProductsInModal} /> */}
       </BrowserRouter>
     );
   }
 }
+
+export default App;
