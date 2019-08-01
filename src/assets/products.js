5231e2ee9227a997c1bd8613dc48d1f2888354e6
aftabumer <innocentboy0300@gmail.com>
2019-08-01 21:01:41 +0500

    hard codded data through redux

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,8 +1,8 @@
-export const products = [
+export const product = [
     {
         name: "CSGO",
         description:
-          "Counter-Strike: Global Offensive is a multiplayer first-person shooter video game developed by Hidden Path Entertainment and Valve Corporation.",
+          "Counter-Strike: Global Offensive (CS:GO) is a multiplayer first-person shooter video game developed by Hidden Path Entertainment and Valve Corporation. It is the fourth game in the Counter-Strike series and was released for Microsoft Windows, OS X, Xbox 360, and PlayStation 3 on August 21, 2012, while the Linux version was released in 2014.",
           price: '15$',
         image: require("./img/csgo.jpg")
       },
@@ -27,88 +27,33 @@ export const products = [
           price: '20$',
         image: require("./img/fortnite.jpg")
       },
-      {
-        name: "APEX LEAGEND",
-        description:
-          "Apex Legends is a free-to-play battle royale game developed by Respawn Entertainment and published by Electronic Arts. It was released for Microsoft Windows, PlayStation 4, and Xbox One",
-          price: '5$',
-          image: require("./img/apex.jpg")
-      },
-      {
-        name: "OVER WATCH",
-        description:
-          "Overwatch is a team-based multiplayer first-person shooter developed and published by Blizzard Entertainment and released on May 24, 2016 for PlayStation 4, Xbox One, and Windows.",
-          price: '55$',
-          image: require("./img/overwatch.jpeg")
-      },
-      {
-        name: "DOTA 2",
-        description:
-          "Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne.",
-          price: '10$',
-          image: require("./img/dota.jpg")
-      },
-      {
-        name: "CYBER PUNK 2070",
-        description:
-          "Cyberpunk 2077 is an upcoming role-playing video game developed and published by CD Projekt, releasing for Microsoft Windows, PlayStation 4, and Xbox One on 16 April 2020.",
-          price: '80$',
-          image: require("./img/cyber.jpg")
-      },
-      {
-        name: "CSGO",
-        description:
-          "Counter-Strike: Global Offensive is a multiplayer first-person shooter video game developed by Hidden Path Entertainment and Valve Corporation.",
-          price: '15$',
-        image: require("./img/csgo.jpg")
-      },
-      {
-        name: "PUBG",
-        description:
-          "PlayerUnknown's Battlegrounds is an online multiplayer battle royale game developed and published by PUBG Corporation, a subsidiary of South Korean video game company Bluehole.",
-          price: '30$',
-          image: require("./img/pubg.jpg")
-      },
-      {
-        name: "GTA V",
-        description:
-          "Grand Theft Auto V is an action-adventure video game developed by Rockstar North and published by Rockstar Games. It was released in September 2013 for PlayStation 3 and Xbox 360,",
-          price: '60$',
-          image: require("./img/gta.jpg")
-      },
-      {
-        name: "FORTNITE",
-        description:
-          "Fortnite is an online video game developed by Epic Games and released in 2017. It is available in three distinct game mode versions that otherwise share the same general gameplay and game engine",
-          price: '20$',
-        image: require("./img/fortnite.jpg")
-      },
-      {
-        name: "APEX LEAGEND",
-        description:
-          "Apex Legends is a free-to-play battle royale game developed by Respawn Entertainment and published by Electronic Arts. It was released for Microsoft Windows, PlayStation 4, and Xbox One",
-          price: '5$',
-          image: require("./img/apex.jpg")
-      },
-      {
-        name: "OVER WATCH",
-        description:
-          "Overwatch is a team-based multiplayer first-person shooter developed and published by Blizzard Entertainment and released on May 24, 2016 for PlayStation 4, Xbox One, and Windows.",
-          price: '55$',
-          image: require("./img/overwatch.jpeg")
-      },
-      {
-        name: "DOTA 2",
-        description:
-          "Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne.",
-          price: '10$',
-          image: require("./img/dota.jpg")
-      },
-      {
-        name: "CYBER PUNK 2070",
-        description:
-          "Cyberpunk 2077 is an upcoming role-playing video game developed and published by CD Projekt, releasing for Microsoft Windows, PlayStation 4, and Xbox One on 16 April 2020.",
-          price: '80$',
-          image: require("./img/cyber.jpg")
-      },
+    //   // {
+    //   //   name: "APEX LEAGEND",
+    //   //   description:
+    //   //     "Apex Legends is a free-to-play battle royale game developed by Respawn Entertainment and published by Electronic Arts. It was released for Microsoft Windows, PlayStation 4, and Xbox One",
+    //   //     price: '5$',
+    //   //     image: require("./img/apex.jpg")
+    //   // },
+    //   // {
+    //   //   name: "OVER WATCH",
+    //   //   description:
+    //   //     "Overwatch is a team-based multiplayer first-person shooter developed and published by Blizzard Entertainment and released on May 24, 2016 for PlayStation 4, Xbox One, and Windows.",
+    //   //     price: '55$',
+    //   //     image: require("./img/overwatch.jpeg")
+    //   // },
+    //   // {
+    //   //   name: "DOTA 2",
+    //   //   description:
+    //   //     "Dota 2 is a multiplayer online battle arena video game developed and published by Valve Corporation. The game is a sequel to Defense of the Ancients, which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne.",
+    //   //     price: '10$',
+    //   //     image: require("./img/dota.jpg")
+    //   // },
+    //   // {
+    //   //   name: "CYBER PUNK 2070",
+    //   //   description:
+    //   //     "Cyberpunk 2077 is an upcoming role-playing video game developed and published by CD Projekt, releasing for Microsoft Windows, PlayStation 4, and Xbox One on 16 April 2020.",
+    //   //     price: '80$',
+    //   //     image: require("./img/cyber.jpg")
+    //   // },
+      
     ]
