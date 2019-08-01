55cb36e20a23ba2f2f859cf8dcca9abfd9e43018
aftabumer <innocentboy0300@gmail.com>
2019-08-01 21:04:09 +0500

    redux implemention done

--- src/store/action/productAction.js
+++ src/store/action/productAction.js
@@ -1,6 +1,13 @@
-import { GET_PRODUCT } from './../constant'
+import { ADD_PRODUCT, GET_PRODUCT } from './../constant'
 
 export class productAction {
+    static addProduct(payload) {
+        return{
+            type: ADD_PRODUCT,
+            payload
+        }
+    }
+
     static getProduct(payload) {
         return{
             type: GET_PRODUCT,
--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -1,33 +1,36 @@
-import {GET_PRODUCT,
-    GET_PRODUCT_SUCCESS,
-    GET_PRODUCT_FAILURE} from './../constant'
-import {products} from '../../assets/products'
+import {
+  ADD_PRODUCT,
+  GET_PRODUCT,
+
+  GET_PRODUCT_FAILURE
+} from "./../constant";
+import { product } from "../../assets/products";
 
 const initialState = {
-    products: [],
-    product: products,
-    getProductsLoader: false,
-    getProductsError: false,
-}
+  products: product,
+  // getProductsLoader: false,
+  // getProductsError: false
+};
 
-export default function productReducer(state= initialState, action){
-    switch(action.type) {
-        case GET_PRODUCT:
-            return{
-                ...state,
-                products: [...state.products, action.payload],
-            }
-        case GET_PRODUCT_SUCCESS:
-            return{
+export default function productReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        products: [...state.products, action.payload]
+      };
+    case GET_PRODUCT:
+      return {
+        ...state,
+        products: [...state.products, action.payload]
+        
+      };
 
-            }
-        case GET_PRODUCT_FAILURE:
-            return{
 
-            }
+    case GET_PRODUCT_FAILURE:
+      return {};
 
-        default:
-            return state
-
-    }
+    default:
+      return state;
+  }
 }
